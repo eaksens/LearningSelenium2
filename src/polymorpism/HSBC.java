eb60cd85bc9dc87646adfe4868e854692a2cc847
Mai Eaksen <mai.eaksen@gfs.com>
2019-12-17 16:43:14 -0500

    4th commit

--- src/polymorpism/HSBC.java
+++ src/polymorpism/HSBC.java
@@ -15,10 +15,21 @@ public class HSBC extends RBI {
 	public double getHomeLoanROI() {
 		return 13.33;
 	}
+	
+	//return type of classname
+	//Method to get the objects of this class
+	public HSBC getOj() {
+		return 
+	}
+	
 	/*
 	 * Overriding is the runtime polymorphism
 	 * Overriding /dynamic binding /late binding /runtime polymorphism
-	 * 
+	 * Interview Questions:
+	 * Q1: Can we change the return type while performing overriding?
+	 * Ans: 
+	 * No -when return type are Primitive
+	 * Yes -when return type is the ClassName -object
 	 */
 
 }
